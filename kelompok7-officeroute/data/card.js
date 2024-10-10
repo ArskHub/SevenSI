48dd9de34945fb2ccbf3afd017f976cbc878446a
QousulHaq <aqousulhaq@gmail.com>
2024-10-10 22:51:15 +0700

    membuat dynamic-card untuk seluruh halaman

---
+++ kelompok7-officeroute/data/card.js
@@ -0,0 +1,103 @@
+export const courseCardData = [
+    {
+        image: "images/ppt.jpg",
+        title: "Learn Microsoft Office PowerPoint (Beginner)",
+        instructor: "John Doe",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        rating: 5.0,
+        price: "Rp. 999.999"
+    },
+    {
+        image: "images/excel.jpg",
+        title: "Learn Microsoft Excel (Intermediate)",
+        instructor: "Jane Smith",
+        description: "Master the skills to excel at Microsoft Excel.",
+        rating: 4.8,
+        price: "Rp. 1.200.000"
+    },
+    {
+        image: "images/word.jpg",
+        title: "Learn Microsoft Word (Advanced)",
+        instructor: "Alice Johnson",
+        description: "Advanced Microsoft Word tips and tricks.",
+        rating: 4.9,
+        price: "Rp. 950.000"
+    },
+    {
+        image: "images/ppt.jpg",
+        title: "Learn Microsoft Office PowerPoint (Beginner)",
+        instructor: "John Doe",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        rating: 5.0,
+        price: "Rp. 999.999"
+    }
+];
+
+export const servicesData = [
+    {
+        icon: "images/ser3.jpg",
+        title: "Best Instructor",
+        description: "Get direct guidance from experienced instructors in their fields and learning materials that are up-to-date and relevant.",
+    },
+    {
+        icon: "images/ser1.jpg",
+        title: "Video Learning",
+        description: "An organized video learning and access to learning materials anytime, anywhere.",
+    },
+    {
+        icon: "images/ser2.jpg",
+        title: "Certification",
+        description: "Official certificates as proof of competence and a transparent and reliable certification process.",
+    }
+];
+
+export const allCourseCardData = [
+    {
+        image: "images/ppt.jpg",
+        title: "Learn Microsoft Office PowerPoint (Beginner)",
+        instructor: "John Doe",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        rating: 5.0,
+        price: "Rp. 999.999"
+    },
+    {
+        image: "images/excel.jpg",
+        title: "Learn Microsoft Excel (Intermediate)",
+        instructor: "Jane Smith",
+        description: "Master the skills to excel at Microsoft Excel.",
+        rating: 4.8,
+        price: "Rp. 1.200.000"
+    },
+    {
+        image: "images/word.jpg",
+        title: "Learn Microsoft Word (Advanced)",
+        instructor: "Alice Johnson",
+        description: "Advanced Microsoft Word tips and tricks.",
+        rating: 4.9,
+        price: "Rp. 950.000"
+    },
+    {
+        image: "images/ppt.jpg",
+        title: "Learn Microsoft Office PowerPoint (Beginner)",
+        instructor: "John Doe",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        rating: 5.0,
+        price: "Rp. 999.999"
+    },
+    {
+        image: "images/excel.jpg",
+        title: "Learn Microsoft Excel (Intermediate)",
+        instructor: "Jane Smith",
+        description: "Master the skills to excel at Microsoft Excel.",
+        rating: 4.8,
+        price: "Rp. 1.200.000"
+    },
+    {
+        image: "images/word.jpg",
+        title: "Learn Microsoft Word (Advanced)",
+        instructor: "Alice Johnson",
+        description: "Advanced Microsoft Word tips and tricks.",
+        rating: 4.9,
+        price: "Rp. 950.000"
+    }
+];
---
+++ kelompok7-officeroute/dynamic-card.js
@@ -0,0 +1,94 @@
+import { courseCardData, servicesData, allCourseCardData } from "./data/card.js";
+
+const courseCard = (course) => {
+    return `
+                <div class="course-card card bg-light rounded-3 shadow overflow-hidden text-center border-0 mb-4">
+                    <img src="${course.image}" class="course-image" alt="Course Image" />
+                    <div class="card-body course-content p-3">
+                        <h5 class="card-title mb-3 fw-bold">${course.title}</h5>
+                        <div class="card-text">
+                            <p class="mb-3">${course.instructor}</p>
+                            <p class="course-description mb-3">${course.description}</p>
+                        </div>
+                        <div class="rating-price d-flex justify-content-between align-items-center mb-3">
+                            <div class="rating text-warning">
+                                <span>${course.rating}</span>
+                                <span class="stars">★★★★★</span>
+                            </div>
+                            <div class="price fw-bold text-success">
+                                <span>${course.price}</span>
+                            </div>
+                        </div>
+                        <div class="d-grid col-6 mx-auto">
+                            <a href="#" class="btn btn-course py-2 text-white mt-3">Buy Now</a>
+                        </div>
+                    </div>
+                </div>
+            `;
+}
+
+const servicesCard = (data) => {
+    return `
+        <div class="col-md-4 mb-4">
+            <div class="service-card text-center h-100">
+                <img src="${data.icon}" alt="Video Learning" class="service-icon"/>
+                <h3>${data.title}</h3>
+                <p>${data.description}</p>
+            </div>
+        </div>
+            `;
+}
+
+const allCoursesCard = (data) => {
+    return `
+        <div class="col">
+          <div class="course-card card rounded-3 overflow-hidden text-center w-100 h-100">
+            <img src="${data.image}" class="course-image" alt="Course Image" />
+            <div class="card-body course-content py-3 px-4">
+              <h5 class="card-title mb-3 fw-bold">
+                ${data.title}
+              </h5>
+              <div class="card-text">
+                <p class="mb-3">${data.instructor}</p>
+                <p class="course-description mb-3">
+                  ${data.description}
+                </p>
+              </div>
+              <div class="rating-price d-flex justify-content-between align-items-center mb-3">
+                <div class="rating text-warning">
+                  <span>${data.rating}</span>
+                  <span class="stars text-warning">★★★★★</span>
+                </div>
+                <div class="price fw-bold text-success d-flex flex-column align-items-end">
+                  <span>Harga Course</span>
+                  <span>${data.price}</span>
+                </div>
+              </div>
+              <div class="d-grid col-6 mx-auto">
+                <a href="#" class="btn btn-course py-2 text-white mt-3">Beli Course Sekarang</a>
+              </div>
+            </div>
+          </div>
+        </div>
+            `;
+}
+
+const generateCard = (data, card, container) => {
+    data.forEach((value) => {
+        container.innerHTML += card(value)
+    })
+}
+
+const cardContainer = document.querySelector('.courses-container');
+const servicesContainer = document.querySelector('.services-container');
+const allCoursesContainer = document.querySelector('.all-courses-container');
+
+if (cardContainer) {
+    generateCard(courseCardData, courseCard, cardContainer)   
+}
+if (servicesContainer) {
+    generateCard(servicesData, servicesCard, servicesContainer)   
+}
+if (allCoursesContainer) {
+    generateCard(allCourseCardData, allCoursesCard, allCoursesContainer)   
+}
